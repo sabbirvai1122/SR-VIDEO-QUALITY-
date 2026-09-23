2eee7339e157637ac963027c4848751d70ec1a77
sabbirvai1122 <rush20470@gmail.com>
2026-09-23 13:00:02 +0600

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ BACKUP_BOT_TOKEN = environ.get('BACKUP_BOT_TOKEN', "")
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ' -1004450462812'))
 
 # Mongodb Database For User Link Click Count Etc Data Store.
-MONGODB_URI = environ.get("MONGODB_URI", "mongodb+srv://sabbirvai1122:SABBIRVAI1122@cluster0.t1bsiei.mongodb.net/?appName=Cluster0
+MONGODB_URI = environ.get("MONGODB_URI", "mongodb+srv://sabbirvai1122:SABBIRVAI1122@cluster0.t1bsiei.mongodb.net/?appName=Cluster0")
 ")
 
 # Stream Url Means Your Deploy Server App Url, Here You Media Will Be Stream And Ads Will Be Shown.
