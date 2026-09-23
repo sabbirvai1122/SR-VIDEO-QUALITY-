5c0efd212cd6cc573437ac9c8bed27cdcc784494
sabbirvai1122 <rush20470@gmail.com>
2026-09-23 13:09:25 +0600

    Update info.py

--- info.py
+++ info.py
@@ -24,7 +24,7 @@ LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1004450462812'))
 MONGODB_URI = environ.get("MONGODB_URI", "mongodb+srv://sabbirvai1122:SABBIRVAI1122@cluster0.t1bsiei.mongodb.net/?appName=Cluster0")
 
 # Stream Url Means Your Deploy Server App Url, Here You Media Will Be Stream And Ads Will Be Shown.
-STREAM_URL = environ.get("STREAM_URL", "")
+STREAM_URL = environ.get("STREAM_URL", "https://sr-video-quality-2.onrender.com")
 
 # This Link Used As Permanent Link That If Your Deploy App Deleted Then You Change Stream Url, So This Link Will Redirect To Stream Url.
 LINK_URL = environ.get("LINK_URL", "https://anime112266.blogspot.com/p/ss-anime-box.html")
